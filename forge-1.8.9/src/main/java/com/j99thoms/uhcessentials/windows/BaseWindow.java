075242fc793b199f5dbe1a6e5a9b33df1cd1e115
j99thoms <97199193+j99thoms@users.noreply.github.com>
2026-03-21 14:45:47 -0700

    feat: port BaseWindow, FileManager, and WindowManager
    - note: renamed 'DefaultWindow' abstract class to 'BaseWindow'

---
+++ forge-1.8.9/src/main/java/com/j99thoms/uhcessentials/windows/BaseWindow.java
@@ -0,0 +1,58 @@
+package com.j99thoms.uhcessentials.windows;
+
+public abstract class BaseWindow {
+
+    public abstract void update();
+
+    public abstract void render();
+
+    public abstract void setToDefault();
+
+    public abstract void setX(int x);
+
+    public abstract void setY(int y);
+
+    public abstract int getX();
+
+    public abstract int getY();
+
+    public abstract int getWidth();
+
+    public abstract int getHeight();
+
+    public abstract void setRGBA(int r, int g, int b, int a);
+
+    public abstract int getR();
+
+    public abstract int getG();
+
+    public abstract int getB();
+
+    public abstract int getA();
+
+    public abstract void setBorderRGB(int r, int g, int b, int a);
+
+    public abstract int getBorderR();
+
+    public abstract int getBorderG();
+
+    public abstract int getBorderB();
+
+    public abstract int getBorderA();
+
+    public abstract void setThickness(float t);
+
+    public abstract double getThickness();
+
+    public abstract void save();
+
+    public abstract void load();
+
+    public abstract String getName();
+
+    public abstract void toggle();
+
+    public abstract int getToggled();
+
+    public abstract String getToolTip();
+}
